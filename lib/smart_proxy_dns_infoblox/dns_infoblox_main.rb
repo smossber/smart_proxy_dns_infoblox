2a90d9f402bb2fdfc4478f47ecb44706c5247988
root <root@nrkxpsat001.migrationsverket.se>
2017-01-26 17:35:00 +0100

    Added skips if infoblox can't find A or PTR records, instead of throwing errors \n Commented out dns_find lookups as these will cause 75 second delays for some reason

--- lib/smart_proxy_dns_infoblox/dns_infoblox_main.rb
+++ lib/smart_proxy_dns_infoblox/dns_infoblox_main.rb
@@ -5,6 +5,7 @@ module Proxy::Dns::Infoblox
   class Record < ::Proxy::Dns::Record
     include Proxy::Log
     include Proxy::Util
+	# THIS IS THE FORK
 
     attr_reader :example_setting, :optional_path, :required_setting, :required_path
 
@@ -19,15 +20,17 @@ module Proxy::Dns::Infoblox
 
     # Calls to these methods are guaranteed to have non-nil parameters
     def create_a_record(fqdn, ip)
-      raise(Proxy::Dns::Collision, "#{fqdn} is already in use") if dns_find(fqdn)
-
+	logger.debug "Skipping FQDN check"
+	#logger.debug "Starting check if the FQDN is already in use"
+      #raise(Proxy::Dns::Collision, "#{fqdn} is already in use") if dns_find(fqdn)
       a_record = Infoblox::Arecord.new(connection: @conn, name: fqdn, ipv4addr: ip)
       a_record.post
     end
 
     def create_ptr_record(fqdn, ip)
       #ip here comes in as 2.1.168.192.in-addr.arpa, so we need to strip and reverse.
-      raise(Proxy::Dns::Collision, "#{ip} is already in use") if dns_find(ip)
+      logger.debug "Skipping FQDN check"
+      #raise(Proxy::Dns::Collision, "#{ip} is already in use") if dns_find(ip)
       fixed_ip = ip.chomp('.in-addr.arpa').split('.').reverse .join('.')
       ptr_record = Infoblox::Ptr.new(connection: @conn, ptrdname: fqdn, ipv4addr: fixed_ip)
 
@@ -39,16 +42,32 @@ module Proxy::Dns::Infoblox
 
     def remove_a_record(fqdn)
       a_record = Infoblox::Arecord.find( @conn, {name: fqdn}).first
-      a_record.delete || raise(Proxy::Dns::NotFound.new("Cannot find DNS entry for #{fqdn}"))
+	# EDIT
+	# checks if the record could be fetched and is not null
+	# If it couldn't, then don't do anything instead of failing
+	unless a_record.nil?
+      #a_record.delete || raise(Proxy::Dns::NotFound.new("Cannot find DNS entry for #{fqdn}"))
+      		a_record.delete
+	else
+		logger.info "Couldn't find A record, skipping"
+	end
     end
 
     def remove_ptr_record(ip)
       #ip here comes in as 2.1.168.192.in-addr.arpa, so we need to strip and reverse.
       fixed_ip = ip.chomp('.in-addr.arpa').split('.').reverse .join('.')
       ptr_record = Infoblox::Ptr.find(@conn, { ipv4addr: fixed_ip }).first
-      ptr_record.ipv6addr=nil
-      ptr_record.view=nil
-      ptr_record.delete || raise(Proxy::Dns::NotFound.new("Cannot find DNS entry for #{ip}"))
+	# EDIT
+	# checks if the record could be fetched and is not null
+	# If it couldn't, then don't do anything instead of failing
+	unless ptr_record.nil?	
+	      ptr_record.ipv6addr=nil
+	      ptr_record.view=nil
+		ptr_record.delete
+	      #ptr_record.delete || raise(Proxy::Dns::NotFound.new("Cannot find DNS entry for #{ip}"))
+	else
+		logger.debug "Couldn't find PTR record, skipping.."
+	end
       # FIXME: remove the reverse 'PTR' record with ip
       # Raise an error if the IP is not in DNS:
       #   raise Proxy::Dns::NotFound.new("Cannot find DNS entry for #{ip}")
